4879f182029ed02c5f79438c41836168a7c2d977
morrolinux <morrolinux@gmail.com>
2019-05-05 16:44:34 +0200

    alternative methods for rfcomm string read

--- app/src/main/java/com/example/morro/telecomando/Core/BluetoothRfcommHelper.java
+++ app/src/main/java/com/example/morro/telecomando/Core/BluetoothRfcommHelper.java
@@ -4,9 +4,16 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import javax.obex.ClientSession;
@@ -73,6 +80,19 @@ public class BluetoothRfcommHelper {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\0");     //\0 is end of string in c
         return s.hasNext() ? s.next() : "";
     }
+
+
+/*
+    private String convertStreamToString(InputStream stream) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[9999];
+        reader.read(buffer);
+        String s = new String(buffer);
+        buffer = null;
+        return s;
+    }
+*/
 
     public String putAndGet(String text){
         System.out.println("putAndGet: "+text);
