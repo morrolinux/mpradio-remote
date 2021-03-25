a3ed34f0bec46465a2910c8e0a96c4c8c6441c1a
Moreno Razzoli <morrolinux@gmail.com>
2021-03-25 11:16:57 +0100

    refactoring and toString for Song

--- app/src/main/java/com/example/morro/telecomando/Core/Song.kt
+++ app/src/main/java/com/example/morro/telecomando/Core/Song.kt
@@ -8,27 +8,28 @@ import java.util.*
  * data model being displayed by the RecyclerView
  */
 class Song(val title: String, val artist: String, val album: String, val year: String, val itemPath: String) {
-    val json: String
-        get() {
-            val song = JSONObject()
-            try {
-                song.put("title", title)
-                song.put("artist", artist)
-                song.put("album", album)
-                song.put("year", year)
-                song.put("path", itemPath)
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-            return song.toString()
-        }
 
+    fun getJson(): String {
+        val song = JSONObject()
+        song.put("title", title)
+        song.put("artist", artist)
+        song.put("album", album)
+        song.put("year", year)
+        song.put("path", itemPath)
+        return song.toString()
+    }
+
+    override fun toString(): String {
+        return "$title ($artist)"
+    }
+
+    /* works like static functions in java */
     companion object {
         @JvmStatic
-        fun buildDummyTrackList(num: Int): ArrayList<Song> {
+        fun buildDummyTrackList(num: Int = 1): ArrayList<Song> {
             val songs = ArrayList<Song>()
             for (i in 1..num) {
-                songs.add(Song("Title", "Artist", "Album", "Year", "path"))
+                songs.add(Song("Title$i", "Artist$i", "Album$i", "Year$i", "path$i"))
             }
             return songs
         }
